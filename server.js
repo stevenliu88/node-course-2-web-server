4d089a92c8e35a9c04372807ed6924bbbff83a28
LL\steven <liuleiwangyue@gmail.com>
2018-08-31 15:58:47 +1000

    inital commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine','hbs');
+// app.use((req,res,next)=>{
+//     res.render('maintance.hbs');
+  
+// });
+app.use(express.static(__dirname +'/public'));
+app.use((req,res,next) =>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error)=>{
+        if(error){
+            console.log('Unable to append that file');
+        }
+    })
+    next();
+});
+
+
+hbs.registerHelper('currentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+})
+app.get('/',(req,res)=>{
+    //res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        purpose: `Build for emily`
+    });
+});
+
+
+
+
+app.get('/about',(req,res)=>{
+     res.render('about.hbs',{
+         pageTitle: `Lei's Page`
+     });
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        name: 'lei',
+        wife: 'yue',
+        daughter: 'emily'
+    })
+});
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000');
+});
